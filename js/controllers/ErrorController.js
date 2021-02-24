959aa083c3530cd45d6d1aeb058348aae7cc0d8c
Fernando Lopez Trejo <fernando_lt@hotmail.es>
2021-02-24 17:39:47 +0100

    Se afina un poco el control de errores, añadiendo una página 404 en caso de not found

--- js/controllers/ErrorController.js
+++ js/controllers/ErrorController.js
@@ -1,12 +1,17 @@
 import BaseController from './BaseController.js'
 import {errorView} from '../views/errorView.js'
 
-
 export default class ErrorController extends BaseController {
 
-    showError(error) {
-        this.element.innerHTML = errorView(error)
-        this.element.classList.remove('hidden')
+    constructor(element) {
+        super(element)
+        this.attachEventListener()
+        this.subscribe(this.events.ERROR, (error) => {
+            this.showError(error)
+        })
+    }
+
+    attachEventListener() {
         this.element.addEventListener('click', (event) => {
             if(event.target == this.element || event.target.classList.contains('delete')) {
                 this.element.classList.add('hidden')
@@ -18,4 +23,15 @@ export default class ErrorController extends BaseController {
             }
         })
     }
-}
+
+    showError(error) {
+        if(error.status === 404) {
+            window.location.href = '/404.html'
+        } else {
+            this.element.innerHTML = errorView(error)
+            this.element.classList.remove('hidden')
+            this.attachEventListener()
+        }
+        
+    }
+}   
